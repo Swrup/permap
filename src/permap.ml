c2d4d03555c3646d9c19bf2f74bdc64d216b0147
Swrup <swrup@protonmail.com>
2022-03-08 18:48:12 +0100

    fix int_of_string on input

--- src/permap.ml
+++ src/permap.ml
@@ -269,13 +269,15 @@ let profile_post request =
           "Your display nick was updated!"
       | Error e -> render_unsafe e request )
     | `Ok [ ("content", content); ("count", count); ("label", label) ] -> (
-      let count = int_of_string count in
-      match User.update_metadata count label content nick with
-      | Ok () ->
-        Dream.respond ~status:`See_Other
-          ~headers:[ ("Location", "/profile") ]
-          "Your display nick was updated!"
-      | Error e -> render_unsafe e request )
+      match int_of_string_opt count with
+      | None -> render_unsafe "Error: invalid count" request
+      | Some count -> (
+        match User.update_metadata count label content nick with
+        | Ok () ->
+          Dream.respond ~status:`See_Other
+            ~headers:[ ("Location", "/profile") ]
+            "Your display nick was updated!"
+        | Error e -> render_unsafe e request ) )
     | `Ok _ | `Many_tokens _ | `Missing_token _ | `Invalid_token _
     | `Wrong_session _ | `Expired _ | `Wrong_content_type -> (
       match%lwt Dream.multipart request with
